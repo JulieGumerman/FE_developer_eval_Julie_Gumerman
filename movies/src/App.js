22ca30d7f4963d87729df7019af63a15a8693bf2
bodhibrdr <julie.gumerman@gmail.com>
2020-04-08 16:53:22 -0600

    react app created; blank slate with a sanity check

---
+++ movies/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="content-wrapper">
+      <h1>Movies</h1>
+    </div>
+  );
+}
+
+export default App;
