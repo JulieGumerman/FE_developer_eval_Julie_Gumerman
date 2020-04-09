89595e67cd1508b8ffff2ab456d7d33b54c073c3
bodhibrdr <julie.gumerman@gmail.com>
2020-04-09 14:31:10 -0600

    axios call

--- movies/src/App.js
+++ movies/src/App.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.scss';
+import axios from 'axios';
+
+
+
 
 function App() {
+
+const getMovies = () => {
+  axios.get("https://api.themoviedb.org/3/movie/550?api_key=727eb794d2d99f0338a6306c81041acc")
+    .then(response => console.log(response))
+    .catch(err => console.log(err))
+}
+
+useEffect(() => {
+
+  getMovies()
+
+}, [])
+
   return (
     <div className="content-wrapper">
       <h1>Movies</h1>
