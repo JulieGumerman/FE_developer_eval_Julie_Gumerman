9fbf263f89fcb7fee901dd765d6551551eab596b
bodhibrdr <julie.gumerman@gmail.com>
2020-04-10 10:38:33 -0600

    navigational functionality complete; basic get requests complete

--- movies/src/Components/MovieDetails.js
+++ movies/src/Components/MovieDetails.js
@@ -1,22 +1,32 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom"
 
 const MovieDetails = ({match}) => {
-    console.log("MATCH PARAMS", match)
-    console.log("MATCH PARAMS ID", match.params.id)
+    const [movie, setMovie] = useState({})
+
+
+    let movie_id = match.params.id
 
     const getMoviesById = (id) => {
         axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=727eb794d2d99f0338a6306c81041acc`)
-            .then(movie => console.log(movie))
+            .then(movie => {
+                setMovie(movie.data)
+            })
             .catch(err => console.log(err))
     }
 
     useEffect(() => {
-        getMoviesById(match.params.id)
+        getMoviesById(movie_id)
     }, [])
-    
+
+    console.log("MOVIE DATA!!!", movie)
     return (
-        <h1>Hello from movie details screen!!!</h1>
+        <div>
+            <h1>{movie.title}</h1>
+            <p>{movie.overview}</p>
+            <button><Link to={"/"}>Return to movies</Link></button>
+        </div>
     );
 }
 
